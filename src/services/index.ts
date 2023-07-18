98ff25d5a0f2804b93339d3ea6f1f2955e7f83c8
milanterhes <terhes.milan@gmail.com>
2023-07-18 14:27:32 +0200

    improve logging & error handling

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,5 +1,6 @@
-import axios from "axios";
+import axios, { AxiosError } from "axios";
 import { z } from "zod";
+import { ProfileStatError } from "../utils";
 
 interface ProfileStat {
   followers: number | null;
@@ -30,18 +31,30 @@ const InstagramProfileSchema = z.object({
 
 export class InstagramService extends ProfileStatService {
   async getProfileStats(handle: string): Promise<ProfileStat> {
-    const result = await axios.get(
-      "https://i.instagram.com/api/v1/users/web_profile_info/?username=" +
-        handle,
-      {
-        // this is a workaround for the instagram api authentication
-        headers: {
-          "User-Agent":
-            "Mozilla/5.0 (Linux; Android 9; GM1903 Build/PKQ1.190110.001; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/75.0.3770.143 Mobile Safari/537.36 Instagram 103.1.0.15.119 Android (28/9; 420dpi; 1080x2260; OnePlus; GM1903; OnePlus7; qcom; sv_SE; 164094539)",
-        },
-      }
-    );
-    const parsed = InstagramProfileSchema.parse(result.data);
+    let result;
+    try {
+      result = await axios.get(
+        "https://i.instagram.com/api/v1/users/web_profile_info/?username=" +
+          handle,
+        {
+          // this is a workaround for the instagram api authentication
+          headers: {
+            "User-Agent":
+              "Mozilla/5.0 (Linux; Android 9; GM1903 Build/PKQ1.190110.001; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/75.0.3770.143 Mobile Safari/537.36 Instagram 103.1.0.15.119 Android (28/9; 420dpi; 1080x2260; OnePlus; GM1903; OnePlus7; qcom; sv_SE; 164094539)",
+          },
+        }
+      );
+    } catch (error) {
+      throw new ProfileStatError("Failed to fetch profile stats", 500, error);
+    }
+
+    let parsed;
+    try {
+      parsed = InstagramProfileSchema.parse(result.data);
+    } catch (error) {
+      throw new ProfileStatError("Failed to parse profile stats", 500, error);
+    }
+
     return {
       followers: parsed.data.user.edge_followed_by.count,
       following: parsed.data.user.edge_follow.count,
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,32 +1,38 @@
 import { Request } from "express";
 import { AnyZodObject, ZodError, z } from "zod";
+import winston from "winston";
 
 export class BaseError extends Error {
   status: number;
-  constructor(message: string, status: number) {
+  constructor(message: string, status: number, originalError?: unknown) {
     super(message);
     this.name = "BaseError";
     this.status = status;
+
+    logger.error(message);
+    if (originalError) {
+      logger.error(originalError);
+    }
   }
 }
 
 export class ValidationError extends BaseError {
-  constructor(message: string, status: number) {
-    super(message, status);
+  constructor(message: string, status: number, originalError?: unknown) {
+    super(message, status, originalError);
     this.name = "ValidationError";
   }
 }
 
 export class UnknownError extends BaseError {
-  constructor(message: string, status: number) {
-    super(message, status);
+  constructor(message: string, status: number, originalError?: unknown) {
+    super(message, status, originalError);
     this.name = "UnknownError";
   }
 }
 
 export class ProfileStatError extends BaseError {
-  constructor(message: string, status: number) {
-    super(message, status);
+  constructor(message: string, status: number, originalError?: unknown) {
+    super(message, status, originalError);
     this.name = "ProfileStatError";
   }
 }
@@ -39,8 +45,24 @@ export async function zParseRequest<T extends AnyZodObject>(
     return schema.parse(req);
   } catch (error) {
     if (error instanceof ZodError) {
-      throw new ValidationError("Invalid request", 400);
+      throw new ValidationError("Invalid request", 400, error);
     }
     throw new UnknownError(JSON.stringify(error), 500);
   }
 }
+
+export const logger = winston.createLogger({
+  level: "info",
+  format: winston.format.json(),
+  transports: [
+    //
+    // - Write all logs with importance level of `error` or less to `error.log`
+    // - Write all logs with importance level of `info` or less to `combined.log`
+    //
+    // new winston.transports.File({ filename: "error.log", level: "error" }),
+    // new winston.transports.File({ filename: "combined.log" }),
+    new winston.transports.Console({
+      format: winston.format.simple(),
+    }),
+  ],
+});
