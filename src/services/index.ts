2d4849db4395a2d7149377e90a72d6c9f0af9fa1
milanterhes <terhes.milan@gmail.com>
2023-07-18 19:37:45 +0200

    try another header

--- src/services/index.ts
+++ src/services/index.ts
@@ -51,6 +51,7 @@ export class InstagramService extends ProfileStatService {
           headers: {
             "User-Agent":
               "Mozilla/5.0 (Linux; Android 9; GM1903 Build/PKQ1.190110.001; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/75.0.3770.143 Mobile Safari/537.36 Instagram 103.1.0.15.119 Android (28/9; 420dpi; 1080x2260; OnePlus; GM1903; OnePlus7; qcom; sv_SE; 164094539)",
+            "X-Ig-App-Id": "936619743392459",
           },
         }
       );
